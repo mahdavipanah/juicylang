c9b264ab1795862d682daeecf447467a313944d5
mahdavipanah <h.mahdavipanah@gmail.com>
2017-07-04 01:36:55 +0430

    Add if statement

--- juicyyacc.py
+++ juicyyacc.py
@@ -54,6 +54,9 @@ colorama_init(autoreset=True)
 variables = {}
 # Contains the last error number happened in runtime environment
 runtime_err = 0
+# A stack containing running states
+# It gets used for disabling interpreting in if and while statements
+running = [True]
 
 
 # Get's a symbol from variables dictionary and shows error if not exists
@@ -104,6 +107,9 @@ def p_stmt(p):
     """
     stmt : PRINT '(' print_arguments ')' ';'
     """
+    if not running[-1]:
+        return
+
     if p[3] is not None:
         print(p[3])
 
@@ -115,6 +121,9 @@ def p_print_arguments(p):
                     | boolexpr
                     | expr
     """
+    if not running[-1]:
+        return
+
     if p[1] is None:
         return
 
@@ -126,6 +135,46 @@ def p_print_arguments(p):
         if p[3] is not None and p[1] is not None:
             p[0] = str(p[1]) + p[3]
 
+
+def p_stmt_if(p):
+    """
+    stmt : IF '(' boolexpr ')' '{' ifA p ifB '}' ELSE '{' ifC p ifB '}'
+    """
+    pass
+
+
+def p_ifA(p):
+    """
+    ifA :
+    """
+    global running
+    # If current state is not running
+    if not running[-1]:
+        running.append(False)
+    # Push evaluated value as running state
+    else:
+        running.append(p[-3])
+
+
+def p_ifB(p):
+    """
+    ifB :
+    """
+    global running
+    running.pop()
+
+
+def p_ifC(p):
+    """
+    ifC :
+    """
+    global running
+    # If current state is not running
+    if not running[-1]:
+        running.append(False)
+    # Push not(evaluated value) as running state
+    else:
+        running.append(not p[-9])
 
 def p_boolexpr_and(p):
     """
@@ -139,6 +188,9 @@ def p_boolexpr_or(p):
     """
     boolexpr : boolexpr OR boolexpr
     """
+    if not running[-1]:
+        return
+
     if p[1] is not None and p[3] is not None:
         p[0] = p[1] or p[3]
 
@@ -147,6 +199,9 @@ def p_boolexpr_not(p):
     """
     boolexpr : NOT boolexpr
     """
+    if not running[-1]:
+        return
+
     if p[2] is not None:
         p[0] = not p[2]
 
@@ -155,6 +210,9 @@ def p_boolexpr_bool(p):
     """
     boolexpr : BOOL expr
     """
+    if not running[-1]:
+        return
+
     p[0] = boolexpr(p[2])
 
 
@@ -162,6 +220,9 @@ def p_boolexpr_bool_boolexpr(p):
     """
     boolexpr : BOOL boolexpr
     """
+    if not running[-1]:
+        return
+
     p[0] = p[2]
 
 
@@ -169,6 +230,9 @@ def p_boolexpr_not_expr(p):
     """
     boolexpr : NOT expr
     """
+    if not running[-1]:
+        return
+
     expr_bool = boolexpr(p[2])
     if expr_bool is not None:
         p[0] = not expr_bool
@@ -178,6 +242,9 @@ def p_boolexpr_paran(p):
     """
     boolexpr : '(' boolexpr ')'
     """
+    if not running[-1]:
+        return
+
     if p[2] is not None:
         p[0] = p[2]
 
@@ -187,6 +254,9 @@ def p_boolexpr_true_false(p):
     boolexpr : TRUE
              | FALSE
     """
+    if not running[-1]:
+        return
+
     p[0] = True if p[1] == 'true' else False
 
 
@@ -194,6 +264,9 @@ def p_boolexpr_comparison(p):
     """
     boolexpr : comparison
     """
+    if not running[-1]:
+        return
+
     p[0] = p[1] if p[1] is not None else None
 
 
@@ -212,6 +285,9 @@ def p_comparison_gt(p):
     """
     comparison : comparisonA '>' expr
     """
+    if not running[-1]:
+        return
+
     if p[1] is None or p[3] is None:
         p[0] = None
     else:
@@ -226,6 +302,9 @@ def p_comparison_lt(p):
     """
     comparison : comparisonA '<' expr
     """
+    if not running[-1]:
+        return
+
     if p[1] is None or p[3] is None:
         p[0] = None
     else:
@@ -240,6 +319,9 @@ def p_comparison_eq(p):
     """
     comparison : comparisonA '=' expr
     """
+    if not running[-1]:
+        return
+
     if p[1] is None or p[3] is None:
         p[0] = None
     else:
@@ -254,6 +336,9 @@ def p_comparison_neq(p):
     """
     comparison : comparisonA NOTEQUAL expr
     """
+    if not running[-1]:
+        return
+
     if p[1] is None or p[3] is None:
         p[0] = None
     else:
@@ -268,6 +353,9 @@ def p_comparison_le(p):
     """
     comparison : comparisonA LE expr
     """
+    if not running[-1]:
+        return
+
     if p[1] is None or p[3] is None:
         p[0] = None
     else:
@@ -282,6 +370,9 @@ def p_comparison_ge(p):
     """
     comparison : comparisonA GE expr
     """
+    if not running[-1]:
+        return
+
     if p[1] is None or p[3] is None:
         p[0] = None
     else:
@@ -297,6 +388,9 @@ def p_comparisonA(p):
     comparisonA : comparison
                 | expr
     """
+    if not running[-1]:
+        return
+
     p[0] = p[1]
 
 
@@ -304,6 +398,9 @@ def p_expr_plus(p):
     """
     expr : expr '+' expr
     """
+    if not running[-1]:
+        return
+
     if p[1] is not None and p[3] is not None:
         p[0] = p[1] + p[3]
 
@@ -312,6 +409,9 @@ def p_expr_minus(p):
     """
     expr : expr '-' expr
     """
+    if not running[-1]:
+        return
+
     if check_int_float_operands(p):
         p[0] = p[1] - p[3]
 
@@ -320,6 +420,9 @@ def p_expr_mul(p):
     """
     expr : expr '*' expr
     """
+    if not running[-1]:
+        return
+
     if check_int_float_operands(p):
         p[0] = p[1] * p[3]
 
@@ -328,6 +431,9 @@ def p_expr_div(p):
     """
     expr : expr '/' expr
     """
+    if not running[-1]:
+        return
+
     if check_int_float_operands(p):
         p[0] = p[1] / p[3]
 
@@ -336,6 +442,9 @@ def p_expr_pow(p):
     """
     expr : expr '^' expr
     """
+    if not running[-1]:
+        return
+
     if check_int_float_operands(p):
         p[0] = pow(p[1], p[3])
 
@@ -344,6 +453,9 @@ def p_expr_symbol(p):
     """
     expr : SYMBOL
     """
+    if not running[-1]:
+        return
+
     # Get symbol's value
     sym_val = get_symbol(p, 1)
     # If symbol does not exist
@@ -357,6 +469,9 @@ def p_expr(p):
          | FLOAT
          | STRING
     """
+    if not running[-1]:
+        return
+
     p[0] = p[1]
 
 
@@ -364,6 +479,9 @@ def p_expr_paran(p):
     """
     expr : '(' expr ')'
     """
+    if not running[-1]:
+        return
+
     p[0] = p[2]
 
 
@@ -371,6 +489,9 @@ def p_expr_unary_minus(p):
     """
     expr : '-' expr %prec UMINUS
     """
+    if not running[-1]:
+        return
+
     if p[2] is not None:
         p[0] = -p[2]
 
@@ -379,6 +500,9 @@ def p_expr_len(p):
     """
     expr : LEN '(' expr ')'
     """
+    if not running[-1]:
+        return
+
     if p[3] is not None:
         if type(p[3]) is str:
             p[0] = len(p[3])
@@ -390,6 +514,9 @@ def p_expr_to_str(p):
     """
     expr : TO_STR '(' expr ')'
     """
+    if not running[-1]:
+        return
+
     if p[3] is not None:
         p[0] = str(p[3])
 
@@ -400,6 +527,9 @@ def p_expr_err(p):
     """
     global runtime_err
 
+    if not running[-1]:
+        return
+
     p[0] = runtime_err
 
 
@@ -408,6 +538,9 @@ def p_expr_to_int(p):
     expr : TO_INT '(' expr ')'
     """
     global runtime_err
+
+    if not running[-1]:
+        return
 
     if p[3] is not None:
         try:
@@ -422,6 +555,9 @@ def p_expr_to_float(p):
     """
     global runtime_err
 
+    if not running[-1]:
+        return
+
     if p[3] is not None:
         try:
             p[0] = float(p[3])
@@ -434,6 +570,9 @@ def p_expr_input(p):
     expr : INPUT '(' expr ')'
          | INPUT '(' ')'
     """
+    if not running[-1]:
+        return
+
     prompt = str(p[3]) if len(p) > 4 else ''
     p[0] = input(prompt)
 
@@ -442,6 +581,9 @@ def p_expr_str_index(p):
     """
     expr : expr '[' expr ']'
     """
+    if not running[-1]:
+        return
+
     if p[1] is None or p[2] is None:
         return
 
@@ -463,6 +605,9 @@ def p_expr_assign(p):
     """
     expr : SYMBOL ASSIGN expr
     """
+    if not running[-1]:
+        return
+
     if p[3] is not None:
         variables[p[1]] = p[3]
         p[0] = p[3]
@@ -472,6 +617,9 @@ def p_expr_str_subscript(p):
     """
     expr : expr '[' expr_or_empty ':' expr_or_empty ']'
     """
+    if not running[-1]:
+        return
+
     if p[1] is None:
         return
 
@@ -494,6 +642,9 @@ def p_expr_or_empty(p):
     expr_or_empty : expr
                   |
     """
+    if not running[-1]:
+        return
+
     if len(p) == 2:
         p[0] = p[1]
 
